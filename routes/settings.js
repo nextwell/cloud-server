c95a665288c1636c2a98f40ff19365657874aa50
nextwell <just.nextwell@gmail.com>
2018-06-06 11:55:15 +0300

    Форма настроек *не закончено

--- routes/settings.js
+++ routes/settings.js
@@ -2,8 +2,11 @@ module.exports = (app, db) => {
 	app.get('/settings', (req, res) => {
 		if ( req.session.userData ){
 			res.redirect('/home');
-		} else{
-			res.redirect('/login');
-		}
+		} else res.redirect('/login');
+	})
+	app.post('/settings', (req, res) => {
+		if ( req.session.userData ){
+
+		} else res.redirect('/login');
 	})
 }
